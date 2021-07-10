444b34082212b97f302754eb24f0e34d6cf02fc1
zihasz <ihaszzeno@gmail.com>
2021-07-10 16:49:11 +0200

    Add relations

---
+++ src/main/java/dev/zihasz/client/manager/relations/Relation.java
@@ -0,0 +1,35 @@
+package dev.zihasz.client.manager.relations;
+
+import com.mojang.authlib.GameProfile;
+
+import java.util.UUID;
+
+public class Relation {
+
+	private final String name;
+	private final UUID uuid;
+	private final RelationType type;
+
+	public Relation(String name, UUID uuid, RelationType type) {
+		this.name = name;
+		this.uuid = uuid;
+		this.type = type;
+	}
+	public Relation(String name, String uuid, RelationType type) {
+		this(name, UUID.fromString(uuid), type);
+	}
+	public Relation(GameProfile profile, RelationType type) {
+		this(profile.getName(), profile.getId(), type);
+	}
+
+	public String getName() {
+		return name;
+	}
+	public UUID getUuid() {
+		return uuid;
+	}
+	public RelationType getType() {
+		return type;
+	}
+
+}
---
+++ src/main/java/dev/zihasz/client/manager/relations/RelationManager.java
@@ -0,0 +1,45 @@
+package dev.zihasz.client.manager.relations;
+
+import dev.zihasz.client.manager.Manager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class RelationManager extends Manager {
+	private static final List<Relation> relations = new ArrayList<>();
+
+	public RelationManager() {
+		relations.add(new Relation(mc.session.getUsername(), mc.session.getPlayerID(), RelationType.FRIEND));
+	}
+
+	public static boolean isFriend(UUID uuid) {
+		for (Relation relation : relations) {
+			if (relation.getType() == RelationType.FRIEND && relation.getUuid().equals(uuid))
+				return true;
+		}
+		return false;
+	}
+	public static boolean isEnemy(UUID uuid) {
+		for (Relation relation : relations) {
+			if (relation.getType() == RelationType.ENEMY && relation.getUuid().equals(uuid))
+				return true;
+		}
+		return false;
+	}
+	public static boolean isAlly(UUID uuid) {
+		for (Relation relation : relations) {
+			if (relation.getType() == RelationType.ALLY && relation.getUuid().equals(uuid))
+				return true;
+		}
+		return false;
+	}
+
+	public static Relation getRelation(UUID uuid) {
+		for (Relation relation : relations) {
+			if (relation.getType() == RelationType.FRIEND && relation.getUuid().equals(uuid))
+				return relation;
+		}
+		return null;
+	}
+}
